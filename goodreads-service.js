997a1c7c83c6afba3280059d6d72690a041a3f70
MarcPuigdemont <marcpuigdemontriera@gmail.com>
2019-03-16 10:23:13 +0100

    Refactored Goodreads service and converted project to npm module

--- goodreads-service.js
+++ goodreads-service.js
@@ -1,34 +1,36 @@
-import "es6-promise";
-import "isomorphic-fetch";
-import parser from "xml2json";
+import 'es6-promise';
+import 'isomorphic-fetch';
+import parser from 'xml2json';
 
+const flatSingle = (arr) => [].concat(...arr);
 export default class GoodreadsService {
-  constructor(key, secret) {
-    this.key = key;
-    this.secret = secret;
-  }
+	constructor(key, secret) {
+		this.key = key;
+		this.secret = secret;
+	}
 
-  async search(title, initPage = 1, numPages = 1) {
-    const url = `https://www.goodreads.com/search.xml?key=${this.key}&q=${title}`;
-    let promiseArray = [];
+	async search(title, initPage = 1, numPages = 1) {
+		const url = `https://www.goodreads.com/search.xml?key=${this.key}&q=${title}`;
+		let promiseArray = [];
 
-    for (let i = initPage; i < initPage + numPages; i++) {
-      let promiseUrl = url + `&page=${i}`;
-      promiseArray.push(fetch(promiseUrl));
-    }
+		for (let i = initPage; i < initPage + numPages; i++) {
+			let promiseUrl = `${url}&page=${i}`;
+			promiseArray.push(fetch(promiseUrl));
+		}
 
-    let responses = await Promise.all(promiseArray);
-    responses = responses.map((response) => response.text());
+		let responses = await Promise.all(promiseArray).then((responses) =>
+			Promise.all(responses.map((response) => response.text()))
+		);
 
-    let res = await Promise.all(responses);
+		let books = responses
+			.map((xmlResponse) => parser.toJson(xmlResponse))
+			.map((jsonResponse) => JSON.parse(jsonResponse))
+			.map((object) => object.GoodreadsResponse.search.results.work)
+			.reduce((books, page) => {
+				// return books.concat(page);
+				return [ ...books, ...page ];
+			}, []);
 
-    let jsonJS = res.map((e) => parser.toJson(e));
-    jsonJS = jsonJS.map((e) => JSON.parse(e));
-
-    const flatSingle = arr => [].concat(...arr);
-    jsonJS = jsonJS.map((e) => e.GoodreadsResponse.search.results.work);
-    jsonJS = flatSingle(jsonJS);
-    
-    return jsonJS;
-  }
+		return books;
+	}
 }
